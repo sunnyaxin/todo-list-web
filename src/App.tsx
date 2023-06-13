8b1877937a7c389f26eaf7a0eee27b325e633bde
Yaxin <sunnyyaxin@gmail.com>
2023-06-13 21:55:38 +0800

    [todo-list] Extract item interface to store info related to item.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
 import React, {FC, useState} from "react";
 import "./App.css";
-import {Button, Container, InputGroup, ListGroup, Navbar, Stack, Form, CloseButton} from "react-bootstrap";
+import {Button, InputGroup, ListGroup, Stack, Form, CloseButton} from "react-bootstrap";
+
+interface Item {
+    id: number,
+    content: string,
+    isCompleted: boolean
+}
 
 export const App: FC = () => {
-  const [todos, setTodos] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState("");
+  const [items, setItems] = useState<Item[]>([]);
+
+  const handleItemChecked = (itemId: number) => {
+    setItems((preItems) => 
+      preItems.map((item) => item.id === itemId? {...item, isCompleted: !item.isCompleted}: item 
+      ));
+  };
   
   return (
     <Stack gap={1}>
-      <Navbar bg="light">
-        <Container>
-          <Navbar.Brand>Best Todo Lists</Navbar.Brand>
-        </Container>
-      </Navbar>
-      <Stack direction="horizontal" gap={3}>
+      <p className="fs-2">Best Todo Lists</p>
+      <Stack direction="horizontal" gap={2}>
         <InputGroup>
           <Form.Control placeholder="Please enter to-do item" value={inputValue} onChange={event => setInputValue(event.target.value)}/>
         </InputGroup>
         <Button onClick={() => {
           setInputValue("");
-          setTodos([inputValue, ...todos]);
+          setItems([
+            {
+              id: items.length+1,
+              content: inputValue,
+              isCompleted: false
+            },
+            ...items
+          ]);
         }}>Add
         </Button>
       </Stack>
       <ListGroup>
-        {todos.map(todo =>
-          <ListGroup.Item key={todo}>
+        {items.map(item =>
+          <ListGroup.Item key={item.id}>
             <Stack direction="horizontal" className="d-flex justify-content-between">
               <Stack direction="horizontal" gap={1}>
-                <Form.Check/>
-                {todo}
+                <Form.Check onChange={() => {handleItemChecked(item.id);}}/>
+                <div className={item.isCompleted?"text-decoration-line-through":"text-decoration-none"}>{item.content}</div>
               </Stack>
               <CloseButton />
             </Stack>
