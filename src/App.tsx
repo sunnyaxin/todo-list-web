d75355cd5180ff4b327527a2eef01636ac04d371
Yaxin <sunnyyaxin@gmail.com>
2023-06-13 17:17:59 +0800

    [todo-list] Add eslint.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React, {FC, useState} from 'react';
-import './App.css';
+import React, {FC, useState} from "react";
+import "./App.css";
 
 export const App: FC = () => {
-  const [todos, setTodos] = useState<string[]>([])
-  const [inputValue, setInputValue] = useState("")
-
-  console.log("render...., todos: " + todos)
-
+  const [todos, setTodos] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState("");
+  
   return (
-      <>
-        <h1>Best Todo Lists</h1>
-        <input type="text" placeholder="Please input to-do item" value={inputValue}
-               onChange={event => setInputValue(event.target.value)}/>
-        <button onClick={() => {
-          setTodos([inputValue, ...todos])
-        }}>Add
-        </button>
-        <ul>
-          {todos.map(todo =>
-              <li key={todo}>{todo}</li>
-          )}
-        </ul>
-      </>
+    <>
+      <h1>Best Todo Lists</h1>
+      <input type="text" placeholder="Please input to-do item" value={inputValue}
+        onChange={event => setInputValue(event.target.value)}/>
+      <button onClick={() => {
+        setInputValue("");
+        setTodos([inputValue, ...todos]);
+      }}>Add
+      </button>
+      <ul>
+        {todos.map(todo =>
+          <li key={todo}>{todo}</li>
+        )}
+      </ul>
+    </>
   );
-}
+};
