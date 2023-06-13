bf64a3fb0b59008d0e5983f89b812107508494cb
Yaxin <sunnyyaxin@gmail.com>
2023-06-13 21:15:55 +0800

    [todo-list] Add bootstrap to implement todo list UI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
 import React, {FC, useState} from "react";
 import "./App.css";
+import {Button, Container, InputGroup, ListGroup, Navbar, Stack, Form, CloseButton} from "react-bootstrap";
 
 export const App: FC = () => {
   const [todos, setTodos] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState("");
   
   return (
-    <>
-      <h1>Best Todo Lists</h1>
-      <input type="text" placeholder="Please input to-do item" value={inputValue}
-        onChange={event => setInputValue(event.target.value)}/>
-      <button onClick={() => {
-        setInputValue("");
-        setTodos([inputValue, ...todos]);
-      }}>Add
-      </button>
-      <ul>
+    <Stack gap={1}>
+      <Navbar bg="light">
+        <Container>
+          <Navbar.Brand>Best Todo Lists</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Stack direction="horizontal" gap={3}>
+        <InputGroup>
+          <Form.Control placeholder="Please enter to-do item" value={inputValue} onChange={event => setInputValue(event.target.value)}/>
+        </InputGroup>
+        <Button onClick={() => {
+          setInputValue("");
+          setTodos([inputValue, ...todos]);
+        }}>Add
+        </Button>
+      </Stack>
+      <ListGroup>
         {todos.map(todo =>
-          <li key={todo}>{todo}</li>
+          <ListGroup.Item key={todo}>
+            <Stack direction="horizontal" className="d-flex justify-content-between">
+              <Stack direction="horizontal" gap={1}>
+                <Form.Check/>
+                {todo}
+              </Stack>
+              <CloseButton />
+            </Stack>
+          </ListGroup.Item>
         )}
-      </ul>
-    </>
+      </ListGroup>
+    </Stack>
   );
 };
