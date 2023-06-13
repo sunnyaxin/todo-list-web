c4e4419ad259c8ea6b93bce7cc22de29921dd30b
Yaxin <sunnyyaxin@gmail.com>
2023-06-13 22:02:52 +0800

    [todo-list] Add handle delete event.

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,28 @@ export const App: FC = () => {
   const [inputValue, setInputValue] = useState("");
   const [items, setItems] = useState<Item[]>([]);
 
+  const handleItemAdded = () => {
+    setInputValue("");
+    setItems([
+      {
+        id: items.length+1,
+        content: inputValue,
+        isCompleted: false
+      },
+      ...items
+    ]);
+  };
+  
   const handleItemChecked = (itemId: number) => {
     setItems((preItems) => 
       preItems.map((item) => item.id === itemId? {...item, isCompleted: !item.isCompleted}: item 
       ));
+  };
+  
+  const handleItemDeleted = (itemId: number) => {
+    setItems((preItems) =>
+      preItems.filter((item) => item.id != itemId)
+    );
   };
   
   return (
@@ -25,18 +43,7 @@ export const App: FC = () => {
         <InputGroup>
           <Form.Control placeholder="Please enter to-do item" value={inputValue} onChange={event => setInputValue(event.target.value)}/>
         </InputGroup>
-        <Button onClick={() => {
-          setInputValue("");
-          setItems([
-            {
-              id: items.length+1,
-              content: inputValue,
-              isCompleted: false
-            },
-            ...items
-          ]);
-        }}>Add
-        </Button>
+        <Button onClick={handleItemAdded}>Add</Button>
       </Stack>
       <ListGroup>
         {items.map(item =>
@@ -46,7 +53,7 @@ export const App: FC = () => {
                 <Form.Check onChange={() => {handleItemChecked(item.id);}}/>
                 <div className={item.isCompleted?"text-decoration-line-through":"text-decoration-none"}>{item.content}</div>
               </Stack>
-              <CloseButton />
+              <CloseButton onClick={() => {handleItemDeleted(item.id);}}/>
             </Stack>
           </ListGroup.Item>
         )}
