1cc7140dd80853195589faf1a5493aafce3e3078
Yaxin <sunnyyaxin@gmail.com>
2023-08-17 21:22:54 +0800

    Cancel edit when click outside the editing item.

--- src/Item.tsx
+++ src/Item.tsx
@@ -1,4 +1,4 @@
-import React, {FC, KeyboardEvent, useState} from "react";
+import React, {FC, KeyboardEvent, useEffect, useRef, useState} from "react";
 import {CloseButton, Form, InputGroup, Stack} from "react-bootstrap";
 
 interface ItemProps {
@@ -10,6 +10,24 @@ export const Item: FC<ItemProps> = ({initContent, handleDeleteItem}) => {
   const [isCompleted, setIsCompleted] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
   const [content, setContent] = useState(initContent);
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (inputRef.current && !inputRef.current.contains(event.target as Node)) {
+        setIsEditing(false);
+      }
+    };
+
+    // 添加事件监听器
+    document.addEventListener("click", handleClickOutside);
+
+    // 移除事件监听器
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
   
   const handleInputKeyUp = (event: KeyboardEvent) => {
     if (event.key === "Enter") {
@@ -20,9 +38,9 @@ export const Item: FC<ItemProps> = ({initContent, handleDeleteItem}) => {
   if(!isCompleted && isEditing)
     return (
       <InputGroup>
-        <Form.Control value={content}
+        <Form.Control value={content} ref={inputRef}
           onChange={event => setContent(event.target.value)}
-          onKeyUp={(event) => {handleInputKeyUp(event);}} //todo cancel edit
+          onKeyUp={(event) => {handleInputKeyUp(event);}}
         />
       </InputGroup>
     );
@@ -31,7 +49,7 @@ export const Item: FC<ItemProps> = ({initContent, handleDeleteItem}) => {
     <Stack direction="horizontal" className="d-flex justify-content-between">
       <Stack direction="horizontal" gap={1}>
         <Form.Check onChange={() => setIsCompleted(!isCompleted)}/>
-        <div onClick={() => {setIsEditing(true);}} 
+        <div onClick={(e) => {setIsEditing(true); e.nativeEvent.stopImmediatePropagation();}}
           className={isCompleted ? "text-decoration-line-through" : "text-decoration-none"}>
           {content}
         </div>
